eccb09fe25ea01e1a6a8a0b466b6b05d862e4412
Lenovo <Lenovo@LAPTOP-13Q7V4JJ>
2022-08-25 14:50:54 +0530

    jira-1212 description project Sunaina7thmayB

---
+++ My1stJavaProject/src/MyJavaProgram/myJavaProg.java
@@ -0,0 +1,16 @@
+package datatypespract;
+
+public class myJavaProg {
+	static int a=10;
+	static int b=20;
+	
+	public void addition() {
+		int c=a+b;
+		System.out.println(c);
+	}
+
+public static void main(String[] args){
+	myJavaProg mathematics = new myJavaProg();
+	mathematics.addition();
+}
+}
